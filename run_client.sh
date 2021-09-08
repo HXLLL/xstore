49a7f457d0e25617c1643f9417b6359fb81ba0a9
Huang Xiaolong <hxl65536@gmail.com>
2021-09-08 11:33:36 +0800

    scripts

--- run_client.sh
+++ run_client.sh
@@ -1,1 +1,1 @@
-env LD_LIBRARY_PATH=./deps/boost/lib:/opt/intel/oneapi/mkl/2021.3.0/lib/intel64 ./ycsb -threads 1 -server_host 192.168.2.1
+sudo env LD_LIBRARY_PATH=./deps/boost/lib:/opt/intel/oneapi/mkl/2021.3.0/lib/intel64 ./ycsb -workloads ycsbb -threads 1 -server_host 192.168.2.1
---
+++ run_master.sh
@@ -0,0 +1,1 @@
+sudo env LD_LIBRARY_PATH=./deps/boost/lib:/opt/intel/oneapi/mkl/2021.3.0/lib/intel64 ./master -client_config cs.toml -epoch 400 -nclients 1
